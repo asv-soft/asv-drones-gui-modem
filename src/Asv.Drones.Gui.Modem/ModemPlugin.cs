78ff3c91ac19f15c0c802e0828483f8ec5fcf9ce
Alexey <248997@gmail.com>
2023-09-01 13:31:54 +0500

    feat: init commit

---
+++ src/Asv.Drones.Gui.Modem/ModemPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using Asv.Drones.Gui.Core;
+
+namespace Asv.Drones.Gui.Modem;
+
+[PluginEntryPoint("Modem", CorePlugin.Name)]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public class ModemPlugin : IPluginEntryPoint
+{
+    [ImportingConstructor]
+    public ModemPlugin()
+    {
+    }
+    public void Initialize()
+    {
+    }
+
+    public void OnFrameworkInitializationCompleted()
+    {
+    }
+
+    public void OnShutdownRequested()
+    {
+    }
+}
---
+++ src/Asv.Drones.Gui.Modem/Shell/Pages/ModemPageView.axaml.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.Composition;
+using Asv.Drones.Gui.Core;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace Asv.Drones.Gui.Modem;
+
+[ExportView(typeof(ModemPageViewModel))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public partial class ModemPageView : ReactiveUserControl<ModemPageViewModel>
+{
+    public ModemPageView()
+    {
+        InitializeComponent();
+    }
+}
